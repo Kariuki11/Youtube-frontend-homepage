aeb7f6466f48120efaf7c9f3c75c1ded92754ec5
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-01 03:28:45 +0300

    New changes

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,8 +1,8 @@
-import { Menu } from "lucide-react"
+import { Bell, Menu, Upload, User } from "lucide-react"
 import logo from "../assets/Logo.jpg"
 import { Button } from "../components/Button"
 export function PageHeader() {
-    return <div className="flex gap-10 lg:gap-20 justify-betwen pt-2">
+    return <div className="flex gap-10 lg:gap-20 justify-betwen pt-2 mb-6 mx-4">
         <div className="flex gap-4 items-center flex-shrink-0">
             <Button variant="ghost" size="icon" className="m-10">
                 <Menu />
@@ -13,6 +13,16 @@ export function PageHeader() {
         
         </div>
         <div></div>
-        <div></div>
+        <div className="flex flex-shrink-0">
+            <Button size="icon" variant="ghost">
+                <Upload />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <Bell />
+            </Button>
+            <Button size="icon" variant="ghost">
+                <User />
+            </Button>
+        </div>
     </div>
 }
