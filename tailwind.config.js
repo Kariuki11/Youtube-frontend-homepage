626616d8a24af75bb2e08ba2267da9d7c3f04059
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-01 04:08:48 +0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 import colors from "tailwindcss/colors"
 
 
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         secondary: {
+//           DEFAULT: colors.neutral[200],
+//           hover: colors.neutral[300],
+//           border: colors.neutral[400],
+//           text: colors.neutral[500],
+//           dark: colors.neutral[800],
+//           ["dark-hover"]: colors.neutral[900]
+//         }
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include all relevant file types
+    "./public/index.html", // Include this if you have any Tailwind classes in your HTML files
+  ],
   theme: {
     extend: {
       colors: {
@@ -13,7 +37,7 @@ module.exports = {
           border: colors.neutral[400],
           text: colors.neutral[500],
           dark: colors.neutral[800],
-          ["dark-hover"]: colors.neutral[900]
+          ["dark-hover"]: colors.neutral[900],
         }
       }
     },
